1717491893c31b7574c871b500eee97a3fdd0cc4
syanzi <963177789@qq.com>
2025-06-07 22:20:16 +0800

    perf:抽离工具组件

--- packages/core/src/pages/home/hooks/useTools.tsx
+++ packages/core/src/pages/home/hooks/useTools.tsx
@@ -10,13 +10,7 @@ import { useAppStore } from '@/stores'
 import imgFeedDiaper from '../assets/img_feed_diaper.png'
 import imgFeedMilk from '../assets/img_feed_milk.png'
 import imgHomeAdd from '../assets/img_home_add.png'
-import imgToolDiaper from '../assets/icon_tool_diaper.png'
-import imgToolMilk from '../assets/icon_tool_milk.png'
-import imgToolSleep from '../assets/icon_tool_sleep.png'
-import imgToolWeight from '../assets/icon_tool_weight.png'
-import imgToolVaccine from '../assets/icon_tool_vaccine.png'
-import imgToolFood from '../assets/icon_tool_food.png'
-import imgToolSupplement from '../assets/icon_tool_supplement.png'
+import { ToolsList } from '../components/tools-list'
 
 import { type IFeedRecord } from '../types'
 import { apiGetLatestFeedRecords } from '../api'
@@ -47,57 +41,6 @@ export const useTools = () => {
     }
   ]
 
-  const moreToolsConfList = [
-    {
-      feedType: EnumFeedType.MILK_BOTTLE,
-      name: '喂奶',
-      icon: imgToolMilk,
-      bgColor: '#FFF7F8',
-      path: '/feed-milk/index'
-    },
-    {
-      feedType: EnumFeedType.DIAPER,
-      name: '尿片',
-      icon: imgToolDiaper,
-      bgColor: '#FAF6FF',
-      path: '/diapering/index'
-    },
-    {
-      feedType: EnumFeedType.HEIGHT_WEIGHT,
-      name: '体重',
-      icon: imgToolWeight,
-      bgColor: '#FFFAF0',
-      path: '/height-weight/index'
-    },
-    {
-      feedType: EnumFeedType.VACCINE,
-      name: '疫苗',
-      icon: imgToolVaccine,
-      bgColor: '#FFF6F7',
-      path: ''
-    },
-    {
-      feedType: EnumFeedType.SUPPLEMENT,
-      name: '补剂',
-      icon: imgToolSupplement,
-      bgColor: '#F9F6FF',
-      path: ''
-    },
-    {
-      feedType: EnumFeedType.SLEEP,
-      name: '睡眠',
-      icon: imgToolSleep,
-      bgColor: '#F4F7FF',
-      path: ''
-    },
-    {
-      feedType: EnumFeedType.BABY_FOOD,
-      name: '辅食',
-      icon: imgToolFood,
-      bgColor: '#FEF9F3',
-      path: ''
-    }
-  ]
   /** 页面显示时获取最新的喂养记录 */
   const getLastList = () => {
     apiGetLatestFeedRecords({
@@ -198,31 +141,7 @@ export const useTools = () => {
       round: true,
       height: '40%',
       render() {
-        return (
-          <div class='home-tools-popup'>
-            {moreToolsConfList.map((tool) => (
-              <div
-                class={`tool-item ${tool.bgColor}`}
-                key={tool.feedType}
-                style={{
-                  backgroundColor: tool.bgColor
-                }}
-                onClick={() => {
-                  if (tool.path === '') {
-                    Taro.showToast({ title: '功能还在开发中,敬请期待!', icon: 'none' })
-                    return
-                  }
-                  navigateTo({
-                    path: '/pages/sub-home' + tool.path
-                  })
-                }}
-              >
-                <Image src={tool.icon} class='tool-item-icon'></Image>
-                <div class='tool-item-name'>{tool.name}</div>
-              </div>
-            ))}
-          </div>
-        )
+        return <ToolsList></ToolsList>
       }
     })
   }
