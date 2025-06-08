d7bd6dd4aea7889425835affaa6feaa5f2fbcbb1
Spig <510034906@qq.com>
2025-06-09 01:29:11 +0800

    feat:处理基础配置

--- packages/core/config/env/stg.ts
+++ packages/core/config/env/stg.ts
@@ -1,5 +1,5 @@
 const metaEnv: MetaEnvType = {
   /** baseUrl */
-  BASE_URL: 'https://api.feiyanyun.com'
+  BASE_URL: 'https://api.xfy-66.com'
 }
 export default metaEnv
--- packages/core/config/env/uat.ts
+++ packages/core/config/env/uat.ts
@@ -1,7 +1,7 @@
 const metaEnv: MetaEnvType = {
   /** baseUrl */
-  //BASE_URL: 'http://192.168.0.102:7202'
-  BASE_URL: 'https://api.feiyanyun.com'
+  BASE_URL: 'http://192.168.0.106:7202'
+  //BASE_URL: 'https://api.xfy-66.com'
 }
 
 export default metaEnv
