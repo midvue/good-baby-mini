c08b4f1aee30d24a82e8ed146dbcbe80ed3e596c
syanzi <963177789@qq.com>
2025-09-12 17:54:08 +0800

    fix:tabbar优化

--- packages/core/src/custom-tab-bar/index.vue
+++ packages/core/src/custom-tab-bar/index.vue
@@ -17,11 +17,12 @@ const isIphoneX = model.includes('iPhone') && screenHeight >= 750
 
 const appStore = useAppStore()
 
-const list = appConfig.tabBar.list.map((item) => {
+const list = appConfig.tabBar.list.map((item, index) => {
   return {
     ...item,
     selectedIconPath: '../' + item.selectedIconPath,
-    iconPath: '../' + item.iconPath
+    iconPath: '../' + item.iconPath,
+    isMiddleItem: index === Math.floor(appConfig.tabBar.list.length / 2)
   }
 })
 
@@ -45,18 +46,24 @@ function onSwitchTab(url: string) {
     <View
       v-for="item in list"
       :key="item.pagePath"
-      class="tab-bar-item"
+      :class="['tab-bar-item', { 'tab-bar-item-middle': item.isMiddleItem }]"
       @tap="onSwitchTab(item.pagePath)"
     >
-      <Image
-        class="tab-bar-item-icon"
-        v-show="selected === item.pagePath"
-        :src="item.selectedIconPath"
-      />
-      <Image class="tab-bar-item-icon" v-show="selected !== item.pagePath" :src="item.iconPath" />
-      <View :style="{ color: selected === item.pagePath ? selectedColor : color }">{{
-        item.text
-      }}</View>
+      <View class="tab-bar-item-content">
+        <Image
+          :class="['tab-bar-item-icon', { 'tab-bar-item-icon-middle': item.isMiddleItem }]"
+          v-show="selected === item.pagePath"
+          :src="item.selectedIconPath"
+        />
+        <Image
+          :class="['tab-bar-item-icon', { 'tab-bar-item-icon-middle': item.isMiddleItem }]"
+          v-show="selected !== item.pagePath"
+          :src="item.iconPath"
+        />
+      </View>
+      <View :style="{ color: selected === item.pagePath ? selectedColor : color }">
+        {{ item.text }}
+      </View>
     </View>
   </View>
 </template>
@@ -65,6 +72,8 @@ function onSwitchTab(url: string) {
 :root,
 .mv-tab-bar {
   --mv-tab-bar-item-height: 52px; /* 底部导航栏条目高度 */
+  --mv-tab-bar-icon-size: 22px; /* 普通图标大小 */
+  --mv-tab-bar-icon-middle-size: 65px; /* 中间图标大小 */
 }
 .mv-tab-bar {
   width: 100%;
@@ -74,6 +83,7 @@ function onSwitchTab(url: string) {
   right: 0;
   background: white;
   display: flex;
+  padding-top: 4px; // 为中间图标留出空间
 
   &.mv-safe-bottom {
     padding-bottom: 20px;
@@ -91,14 +101,38 @@ function onSwitchTab(url: string) {
     height: var(--mv-tab-bar-item-height);
   }
 
+  // 中间项特殊样式
+  .tab-bar-item-middle {
+    // 增加中间项的高度，确保图标可以更大
+    padding-top: -10px;
+  }
+
+  .tab-bar-item-content {
+    // 图标容器，用于控制图标位置
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: var(--mv-tab-bar-icon-size);
+    margin-bottom: 4px;
+  }
+
   .tab-bar-item-icon {
-    width: 22px;
-    height: 22px;
+    width: var(--mv-tab-bar-icon-size);
+    height: var(--mv-tab-bar-icon-size);
     flex: none;
   }
+
+  // 中间图标更大的样式
+  .tab-bar-item-icon-middle {
+    width: var(--mv-tab-bar-icon-middle-size);
+    height: var(--mv-tab-bar-icon-middle-size);
+    // 将中间图标向上偏移，与其他文字保持对齐
+    transform: translateY(-20px);
+  }
+
   .tab-bar-item-tool-selected {
-    width: 22px;
-    height: 22px;
+    width: var(--mv-tab-bar-icon-size);
+    height: var(--mv-tab-bar-icon-size);
     flex: none;
   }
 
